0bf484a21f7134dde1c9f08024a9b8232ed93195
Puthisorn <puthisorn@gmail.com>
2015-07-13 09:33:40 +0700

    First Commit

---
+++ home.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using PhilipGoldIron.Bll;
+
+namespace PhilipGoldIron
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+            InitialProc("");
+            
+        }
+
+        public Boolean ValidateData(string ddd)
+        {
+            bool result = false;
+
+           
+
+            return result;
+        }
+
+        
+        public Boolean InitialProc(string sIDNo)
+        {
+            PhilipGoldIron.Bll.Processing proc = new Processing();
+            bool result = false;
+
+            //Verify ID Number
+            bool test = proc.VerifyPeopleID(sIDNo);
+
+            return result;
+        }
+    }
+}
